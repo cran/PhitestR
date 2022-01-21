775526a0d8d3fc2bc91ea681094142f12105ae34
Wei Vivian Li <vivian.li@rutgers.edu>
2022-01-21 08:13:00 +0000

    version 0.2.0

--- inst/doc/vignette.R
+++ inst/doc/vignette.R
@@ -1,36 +1,23 @@
 ## ----eval = FALSE-------------------------------------------------------------
-#  data = readRDS("Cortex2_count.rds")
+#  data = readRDS("Example.rds")
 #  object = data$count
 #  label = data$labels
 
 ## ----eval = FALSE-------------------------------------------------------------
 #  result = phitest(object, label, ncores = 2)
 #  result$pval
-#  #Oligodendrocyte     Endothelial       Astrocyte
-#  #      0.6107304       0.9999999       0.4844792
-
-## ----eval = FALSE-------------------------------------------------------------
-#  data = readRDS("Cortex2_Seurat.rds")
-#  object = FindClusters(data, resolution = 0.1)
-#  table(object$seurat_clusters)
-
-## ----eval = FALSE-------------------------------------------------------------
-#  result2 = phitest(object, ncores = 2)
-#  result2$pval[1:3]
-#  #           4            1            0
-#  #1.000000e+00 9.997925e-01 1.549529e-08
 
 ## ----eval = FALSE-------------------------------------------------------------
 #  library(ggplot2)
-#  labels = names(result2$par)
+#  labels = names(result$par)
 #  par = lapply(labels, function(l){
-#    tp = result2$par[[l]]
+#    tp = result$par[[l]]
 #    tp$cluster = l
 #    return(tp)
 #  })
 #  par = Reduce(rbind, par)
 #  ggplot(par, aes(x = dhat, y = dhat.c)) +
-#    geom_point(shape = 1) +
+#    geom_point(shape = 1, cex = .5) +
 #    geom_abline(intercept = 0, slope = 1, color = "red") +
 #    facet_wrap(~cluster) +
 #    xlab("Estimated frequency of zero count (gene-specific dispersion)") +
